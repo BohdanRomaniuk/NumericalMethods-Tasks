38de96057c63afc7173aa7c59ad04bf8dd9eff3c
BohdanRomaniuk <bohdan2307@gmail.com>
2017-09-19 19:51:42 +0300

    Uploading Simple iteration and Seidel algorithms due to the private repository

--- 2) Simple Iteration and Seidel SLE/Form1.Designer.cs
+++ 2) Simple Iteration and Seidel SLE/Form1.Designer.cs
@@ -28,12 +28,175 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.label2 = new System.Windows.Forms.Label();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+			this.groupBox3 = new System.Windows.Forms.GroupBox();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.groupBox1.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			this.groupBox3.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(13, 13);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(42, 13);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Метод:";
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+            "Метод Простої Ітерації",
+            "Метод Зейделя"});
+			this.comboBox1.Location = new System.Drawing.Point(61, 8);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(179, 21);
+			this.comboBox1.TabIndex = 3;
+			this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(437, 6);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(107, 23);
+			this.button1.TabIndex = 4;
+			this.button1.Text = "Розвязати СЛАР";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(259, 11);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(53, 13);
+			this.label3.TabIndex = 6;
+			this.label3.Text = "Точність:";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(318, 8);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(83, 20);
+			this.textBox1.TabIndex = 7;
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 5;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(6, 19);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 4;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(389, 134);
+			this.tableLayoutPanel1.TabIndex = 8;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.tableLayoutPanel1);
+			this.groupBox1.Location = new System.Drawing.Point(16, 46);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(402, 166);
+			this.groupBox1.TabIndex = 9;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Система";
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.tableLayoutPanel2);
+			this.groupBox2.Location = new System.Drawing.Point(424, 46);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(144, 166);
+			this.groupBox2.TabIndex = 10;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "Розвязок";
+			// 
+			// tableLayoutPanel2
+			// 
+			this.tableLayoutPanel2.ColumnCount = 2;
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 80F));
+			this.tableLayoutPanel2.Location = new System.Drawing.Point(7, 20);
+			this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+			this.tableLayoutPanel2.RowCount = 4;
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.tableLayoutPanel2.Size = new System.Drawing.Size(131, 133);
+			this.tableLayoutPanel2.TabIndex = 0;
+			// 
+			// groupBox3
+			// 
+			this.groupBox3.Controls.Add(this.richTextBox1);
+			this.groupBox3.Location = new System.Drawing.Point(16, 219);
+			this.groupBox3.Name = "groupBox3";
+			this.groupBox3.Size = new System.Drawing.Size(552, 265);
+			this.groupBox3.TabIndex = 11;
+			this.groupBox3.TabStop = false;
+			this.groupBox3.Text = "Розв\'язування";
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Location = new System.Drawing.Point(7, 20);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(539, 239);
+			this.richTextBox1.TabIndex = 0;
+			this.richTextBox1.Text = "";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "Form1";
+			this.ClientSize = new System.Drawing.Size(586, 496);
+			this.Controls.Add(this.groupBox3);
+			this.Controls.Add(this.groupBox2);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.label2);
+			this.Name = "Form1";
+			this.Text = "Чисельні Методи №2";
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox3.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+		private System.Windows.Forms.GroupBox groupBox3;
+		private System.Windows.Forms.RichTextBox richTextBox1;
 	}
 }
 
--- 2) Simple Iteration and Seidel SLE/Form1.cs
+++ 2) Simple Iteration and Seidel SLE/Form1.cs
@@ -9,11 +9,247 @@ using System.Windows.Forms;
 
 namespace _2__Simple_Iteration_and_Seidel_SLE
 {
+
 	public partial class Form1 : Form
 	{
+		public static double[] multiplyMatrixAndVec(double[,] matrix, double[] vec)
+		{
+			double[] multi = new double[vec.Length];
+			for (uint i = 0; i < matrix.GetLength(0); ++i)
+			{
+				for (uint j = 0; j < matrix.GetLength(1); ++j)
+				{
+					multi[i] += matrix[i, j] * vec[j];
+				}
+			}
+			return multi;
+		}
+		public static string printMatrix<T>(T[,] matrix)
+		{
+			string res = "";
+			for (uint i = 0; i < matrix.GetLength(0); ++i)
+			{
+				for (uint j = 0; j < matrix.GetLength(1); ++j)
+				{
+					res += String.Format("{0}\t", matrix[i, j]);
+				}
+				res += String.Format("\n");
+			}
+			return res;
+		}
+		public static string printMatrix<T>(T[,] matrix, T[] vec)
+		{
+			string res = "";
+			for (uint i = 0; i < matrix.GetLength(0); ++i)
+			{
+				for (uint j = 0; j < matrix.GetLength(1); ++j)
+				{
+					res += String.Format("{0}\t", matrix[i, j]);
+				}
+				res += String.Format(vec[i].ToString());
+				res += String.Format("\n");
+			}
+			return res;
+		}
+		public static string printVec<T>(T[] vec)
+		{
+			string res = "";
+			for (uint i = 0; i < vec.Length; ++i)
+			{
+				res += String.Format("{0}\t", vec[i]);
+			}
+			res += String.Format("\n");
+			return res;
+		}
+		public static double normaVec(double[] vec)
+		{
+			double norma = Math.Abs(vec[0]);
+			for (int i = 1; i < vec.Length; ++i)
+			{
+				if (Math.Abs(vec[i]) > norma)
+				{
+					norma = Math.Abs(vec[i]);
+				}
+			}
+			return norma;
+		}
+
+		private int methodType;
+		private double[,] matrixA;
+		private double[] vectorB;
+		private double precision;
+		private const int size = 4;
 		public Form1()
 		{
 			InitializeComponent();
+			comboBox1.SelectedIndex = 0;
+			methodType = Convert.ToInt32(comboBox1.SelectedIndex);
+			textBox1.Text = "0,001";
+
+			/*
+			matrixA = new double[size, size]
+			{
+				{ 8,1,1,-1 },
+				{ 1,5,-1,-1 },
+				{ 1,-1,5,1 },
+				{ 2,1,-1,10 }
+			};
+			*/
+
+			/*
+			matrixA = new double[size, size]
+			{
+				{ 0.12,-1,0.32,-0.18 },
+				{ 0.08,-0.12,-0.77,0.32 },
+				{ 0.25,0.22,0.14,-1 },
+				{ -0.77,-0.14,0.06,-0.12 }
+			};
+			*/
+
+			matrixA = new double[size, size]
+			{
+				{ 25,3,5,4 },
+				{ 3,25,4,5 },
+				{ 4,5,25,3 },
+				{ 5,4,5,25 }
+			};
+
+			//vectorB = new double[size]{9,4,18,41};
+			//vectorB = new double[size] { 0.72, 0.58, -1.56, -1.21 };
+			vectorB = new double[size] { 1.11, 1.24, 1.55, 1.16 };
+			for (int i = 0; i < size; ++i)
+			{
+				for (int j = 0; j < size; ++j)
+				{
+					tableLayoutPanel1.Controls.Add(new TextBox() { Text = Convert.ToString(matrixA[i, j]) }, j, i);
+				}
+				tableLayoutPanel1.Controls.Add(new TextBox() { Text = Convert.ToString(vectorB[i]) }, 4, i);
+				tableLayoutPanel2.Controls.Add(new Label() { Text = String.Format("x{0}", i + 1) }, 0, i);
+				tableLayoutPanel2.Controls.Add(new TextBox() { }, 1, i);
+			}
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			richTextBox1.Text = "";
+			precision = Convert.ToDouble(textBox1.Text);
+			for (int i = 0; i < size; ++i)
+			{
+				for (int j = 0; j < size; ++j)
+				{
+					matrixA[i, j] = Convert.ToDouble(tableLayoutPanel1.GetControlFromPosition(j, i).Text);
+				}
+				vectorB[i] = Convert.ToDouble(tableLayoutPanel1.GetControlFromPosition(size, i).Text);
+			}
+			double[,] matrixC = new double[size, size];
+			double[] vectorD = new double[size];
+			for (int i = 0; i < size; ++i)
+			{
+				for (int j = 0; j < size; ++j)
+				{
+					if (i == j)
+					{
+						matrixC[i, j] = 0;
+					}
+					else
+					{
+						matrixC[i, j] = -(matrixA[i, j] / matrixA[i, i]);
+					}
+				}
+				vectorD[i] = vectorB[i] / matrixA[i, i];
+			}
+			richTextBox1.Text += "Матриця C:\n" + printMatrix(matrixC);
+			richTextBox1.Text += "\nВектор d:\n" + printVec(vectorD);
+			richTextBox1.Text += "\nПеревірка достатньої умови збіжності:\n";
+			double normaC = 0;
+			for (int i = 0; i < size; ++i)
+			{
+				double sum = 0;
+				for (int j = 0; j < size; ++j)
+				{
+					sum += Math.Abs(matrixC[i, j]);
+				}
+				if (sum > normaC)
+				{
+					normaC = sum;
+				}
+				richTextBox1.Text += String.Format("∑|c{0}j|={1}{2}\n", i + 1, sum, (sum < 1) ? "<1" : ">1");
+			}
+			richTextBox1.Text += String.Format("\nНорма матриці C:\n||C||={0}\n", normaC);
+
+			double[] curX = new double[size];
+			for (uint i = 0; i < size; ++i)
+			{
+				curX[i] = vectorD[i];
+			}
+			richTextBox1.Text += String.Format("\nПочаткове наближення x0=d:\n{0}", printVec(curX));
+			if (methodType == 0)
+			{
+				double[] nextX = new double[size];
+				uint count = 0;
+				double normaNextXCurX = 0;
+				do
+				{
+					double[] CmultCurX = multiplyMatrixAndVec(matrixC, curX);
+					for (int i = 0; i < size; ++i)
+					{
+						nextX[i] = vectorD[i] + CmultCurX[i];
+					}
+					richTextBox1.Text += String.Format("\nНаближення №{0} x{0}=d+C*x{1}:\n{2}", count + 1, count++, printVec(nextX));
+					double[] subXNextXCur = new double[size];
+					for (int i = 0; i < size; ++i)
+					{
+						subXNextXCur[i] = nextX[i] - curX[i];
+						curX[i] = nextX[i];
+					}
+					normaNextXCurX = normaVec(subXNextXCur);
+					richTextBox1.Text += String.Format("Норма ||x{0}-x{1}||={2}\n", count, count - 1, normaNextXCurX);
+				} while (((normaC / (1 - normaC)) * normaNextXCurX) >= precision);
+				richTextBox1.Text += String.Format("\nРозвязок:::::\n{0}\nКількість ітерацій={1}", printVec(nextX), count);
+				richTextBox1.Text += String.Format("\nКількість ітерацій(апостеріорна оцінка): {0}\n", Math.Ceiling((Math.Log10((precision * (1 - normaC)) / (normaVec(vectorD))) / Math.Log10(normaC)) - 1));
+			}
+			else if (methodType == 1)
+			{
+				double[] nextX = new double[size];
+				double[] subXNextXCur = new double[size];
+				uint count = 0;
+				double normaNextXCurX = 0;
+				do
+				{
+					for (int i = 0; i < size; ++i)
+					{
+						double sum1 = 0, sum2 = 0;
+						for (int j = 0; j < i; ++j)
+						{
+							sum1 += matrixC[i, j] * nextX[j];
+						}
+						for (int j = i; j < size; ++j)
+						{
+							sum2 += matrixC[i, j] * curX[j];
+						}
+						nextX[i] = vectorD[i] + sum1 + sum2;
+					}
+					richTextBox1.Text += String.Format("\nНаближення №{0} x{0}=d+C*x{0}+C*x{1}:\n{2}", count + 1, count++, printVec(nextX));
+
+					for (int i = 0; i < size; ++i)
+					{
+						subXNextXCur[i] = nextX[i] - curX[i];
+						curX[i] = nextX[i];
+					}
+					normaNextXCurX = normaVec(subXNextXCur);
+					richTextBox1.Text += String.Format("Норма ||x{0}-x{1}||={2}\n", count, count - 1, normaNextXCurX);
+				} while (normaNextXCurX >= ((normaC / (1 - normaC)) * precision));
+				richTextBox1.Text += String.Format("\nРозвязок:::::\n{0}\nКількість ітерацій={1}", printVec(nextX), count);
+			}
+			for(int i=0; i< size; ++i)
+			{
+				tableLayoutPanel2.GetControlFromPosition(1, i).Text = curX[i].ToString();
+            }
+		}
+
+		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			methodType = Convert.ToInt32(comboBox1.SelectedIndex);
 		}
 	}
 }
