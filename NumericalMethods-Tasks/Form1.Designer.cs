708d97dfd766a28f10e5006d3871e97861911b09
BohdanRomaniuk <bohdan2307@gmail.com>
2017-09-22 22:25:39 +0300

    First upload of Simple Iteration algorigth for linear equation

--- NumericalMethods-Tasks/Form1.Designer.cs
+++ NumericalMethods-Tasks/Form1.Designer.cs
@@ -28,12 +28,207 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.label7 = new System.Windows.Forms.Label();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.groupBox1.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(6, 17);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(32, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "φ(x)=";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(6, 49);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(34, 13);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "φ\'(x)=";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(44, 14);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(146, 20);
+			this.textBox1.TabIndex = 2;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(44, 46);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(146, 20);
+			this.textBox2.TabIndex = 3;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(356, 14);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(91, 20);
+			this.textBox3.TabIndex = 4;
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(356, 46);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(91, 20);
+			this.textBox4.TabIndex = 5;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(459, 14);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 6;
+			this.button1.Text = "Розв\'язати";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(201, 16);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(149, 13);
+			this.label3.TabIndex = 7;
+			this.label3.Text = "Початкове наближення x(0):";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(288, 49);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(62, 13);
+			this.label4.TabIndex = 8;
+			this.label4.Text = "Точність ε:";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(19, 101);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(61, 13);
+			this.label5.TabIndex = 9;
+			this.label5.Text = "Розв\'язок:";
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(86, 98);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(175, 20);
+			this.textBox5.TabIndex = 10;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.richTextBox1);
+			this.groupBox1.Location = new System.Drawing.Point(13, 124);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(540, 359);
+			this.groupBox1.TabIndex = 11;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Розв\'язування";
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Location = new System.Drawing.Point(7, 19);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(527, 334);
+			this.richTextBox1.TabIndex = 0;
+			this.richTextBox1.Text = "";
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.label1);
+			this.groupBox2.Controls.Add(this.textBox1);
+			this.groupBox2.Controls.Add(this.label2);
+			this.groupBox2.Controls.Add(this.button1);
+			this.groupBox2.Controls.Add(this.label4);
+			this.groupBox2.Controls.Add(this.textBox3);
+			this.groupBox2.Controls.Add(this.textBox4);
+			this.groupBox2.Controls.Add(this.textBox2);
+			this.groupBox2.Controls.Add(this.label3);
+			this.groupBox2.Location = new System.Drawing.Point(13, 12);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(540, 76);
+			this.groupBox2.TabIndex = 12;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "Вхідні дані";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(310, 101);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(101, 13);
+			this.label7.TabIndex = 14;
+			this.label7.Text = " Кількість ітерацій:";
+			// 
+			// textBox6
+			// 
+			this.textBox6.Location = new System.Drawing.Point(417, 98);
+			this.textBox6.Name = "textBox6";
+			this.textBox6.Size = new System.Drawing.Size(100, 20);
+			this.textBox6.TabIndex = 15;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "Form1";
+			this.ClientSize = new System.Drawing.Size(565, 495);
+			this.Controls.Add(this.textBox6);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.groupBox2);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.textBox5);
+			this.Name = "Form1";
+			this.Text = "Чисельні методи №1";
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox2.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.TextBox textBox6;
 	}
 }
 
--- NumericalMethods-Tasks/Form1.cs
+++ NumericalMethods-Tasks/Form1.cs
@@ -14,6 +14,50 @@ namespace NumericalMethods_Tasks
 		public Form1()
 		{
 			InitializeComponent();
+			textBox1.Text = "(5/6)*sin(x)";
+			textBox2.Text = "(5/6)*cos(x)";
+			textBox3.Text = "1";
+			textBox4.Text = "0,00001";
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			richTextBox1.Text = "";
+			richTextBox1.Text += "Перевірка достатньої умови збіжності ітераційного процесу:";
+			bool coincides = true;
+			for(double i=1; i<=10; ++i)
+			{
+				double xChecking = (double)5/6*Math.Cos(i);
+				richTextBox1.Text += String.Format("\n |φ'({0})|={1}{2}", i, xChecking, (Math.Abs(xChecking) < 1) ? "<1" : ">1");
+				if (Math.Abs(xChecking) >= 1)
+				{
+					coincides = false;
+					break;
+				}
+			}
+			if (!coincides)
+			{
+				richTextBox1.Text += "\n Достатня умова збіжності |φ'(x)|<1 не виконується!!!!";
+			}
+			else
+			{
+				richTextBox1.Text += "\n Достатня умова збіжності |φ'(x)|<1 виконується.\n";
+				double precision = Convert.ToDouble(textBox4.Text);
+				double prevX = Convert.ToDouble(textBox3.Text);
+				uint count = 0;
+				double curX = 0;
+				double prevXRem;
+				do
+				{
+					curX = (double)5 / 6 * Math.Sin(prevX);
+					richTextBox1.Text += String.Format("\nНаближення №{1} x({1})=φ(x({0}))\nx({1})={2}\n", count, ++count, curX);
+					prevXRem = prevX;
+					prevX = curX;
+				} while (Math.Abs(curX - prevXRem) >= precision);
+				richTextBox1.Text += String.Format("\nРозв'язок:::::::::::\n x={0}\nКількість ітерацій: {1}", Math.Round(curX, 5), count);
+				textBox5.Text = Convert.ToString(Math.Round(curX, 5));
+				textBox6.Text = Convert.ToString(count);
+			}
 		}
 	}
 }
