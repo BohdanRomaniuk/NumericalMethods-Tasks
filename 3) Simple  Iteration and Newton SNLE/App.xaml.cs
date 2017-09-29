4a998e6bad2424485d84c977f14908c74569e9af
BohdanRomaniuk <bohdan2307@gmail.com>
2017-09-29 22:11:39 +0300

    Add desing for 3 project Simple Iteration and Newton angorithms for SLNE using WPF

---
+++ 3) Simple  Iteration and Newton SNLE/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace _3__Simple__Iteration_and_Newton_SNLE
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+	}
+}
