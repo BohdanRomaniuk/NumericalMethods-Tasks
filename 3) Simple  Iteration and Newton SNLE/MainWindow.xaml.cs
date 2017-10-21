33f18a1bff6ca064be4aa98688e95ea0406f6cda
BohdanRomaniuk <bohdan2307@gmail.com>
2017-10-21 19:53:29 +0300

    Add graphic rof precision in Newton Project

--- 3) Simple  Iteration and Newton SNLE/MainWindow.xaml.cs
+++ 3) Simple  Iteration and Newton SNLE/MainWindow.xaml.cs
@@ -33,7 +33,7 @@ namespace _3__Simple__Iteration_and_Newton_SNLE
 			noSpaceStyle.Setters.Add(new Setter(Paragraph.MarginProperty, new Thickness(0)));
 			richTextBox.Resources.Add(typeof(Paragraph), noSpaceStyle);
 		}
-
+		
 		private int getRound(double precision)
 		{
 			int roundCount = 0;
@@ -261,12 +261,110 @@ namespace _3__Simple__Iteration_and_Newton_SNLE
 		//Graphic
 		private void button1_Click(object sender, RoutedEventArgs e)
 		{
+			double ymax = canGraph.Height - 20;
+			double xmax = canGraph.Width - 20;
+			double xCoord = Convert.ToDouble(textBox3.Text);
+			double yCoord = Convert.ToDouble(textBox4.Text);
 
+			PointCollection points1 = new PointCollection();
+			
+			points1.Add(new Point(20, ymax - yCoord*20));
+			points1.Add(new Point(xmax, ymax - yCoord*20));
+
+			Polyline polyline1 = new Polyline();
+            polyline1.StrokeThickness = 1;
+			polyline1.Stroke = Brushes.Red;
+			polyline1.Points = points1;
+
+			PointCollection points2 = new PointCollection();
+
+			points2.Add(new Point(20+xCoord * 20, 20));
+			points2.Add(new Point(20+xCoord * 20, ymax));
+
+			Polyline polyline2 = new Polyline();
+			polyline2.StrokeThickness = 1;
+			polyline2.Stroke = Brushes.Red;
+			polyline2.Points = points2;
+
+			canGraph.Children.Add(polyline1);
+			canGraph.Children.Add(polyline2);
 		}
 
 		private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
 		{
 			methodType = Convert.ToUInt32(comboBox.SelectedIndex);
+		}
+
+		private void LoadGraphic(object sender, RoutedEventArgs e)
+		{
+			const double margin = 20;
+			double xmin = margin;
+			double xmax = canGraph.Width - margin;
+			double ymin = margin;
+			double ymax = canGraph.Height - margin;
+			const double step = 20;
+
+			// Make the X axis.
+			GeometryGroup xaxis_geom = new GeometryGroup();
+			xaxis_geom.Children.Add(new LineGeometry(
+				new Point(0, ymax), new Point(canGraph.Width, ymax)));
+			for (double x = xmin + step; x <= canGraph.Width - step; x += step)
+			{
+				xaxis_geom.Children.Add(new LineGeometry(new Point(x, ymax - margin / 4), new Point(x, ymax + margin / 4)));
+				DrawText(canGraph, (x / 20 - 1).ToString(), new Point(x, ymax + margin / 4), 12, HorizontalAlignment.Center, VerticalAlignment.Top);
+			}
+
+			Path xaxis_path = new Path();
+			xaxis_path.StrokeThickness = 1;
+			xaxis_path.Stroke = Brushes.Black;
+			xaxis_path.Data = xaxis_geom;
+
+			canGraph.Children.Add(xaxis_path);
+
+			// Make the Y ayis.
+			GeometryGroup yaxis_geom = new GeometryGroup();
+			yaxis_geom.Children.Add(new LineGeometry(
+				new Point(xmin, 0), new Point(xmin, canGraph.Height)));
+			int count = 13;
+			for (double y = ymin + step; y <= canGraph.Height - step; y += step)
+			{
+				yaxis_geom.Children.Add(new LineGeometry(new Point(xmin - margin / 4, y), new Point(xmin + margin / 4, y)));
+				DrawText(canGraph, (count--).ToString(), new Point(xmin - margin, y - 35), 12, HorizontalAlignment.Center, VerticalAlignment.Top);
+			}
+
+			Path yaxis_path = new Path();
+			yaxis_path.StrokeThickness = 1;
+			yaxis_path.Stroke = Brushes.Black;
+			yaxis_path.Data = yaxis_geom;
+
+			canGraph.Children.Add(yaxis_path);
+		}
+		private void DrawText(Canvas can, string text, Point location,
+			double font_size,
+			HorizontalAlignment halign, VerticalAlignment valign)
+		{
+			// Make the label.
+			Label label = new Label();
+			label.Content = text;
+			label.FontSize = font_size;
+			can.Children.Add(label);
+
+			// Position the label.
+			label.Measure(new Size(double.MaxValue, double.MaxValue));
+
+			double x = location.X;
+			if (halign == HorizontalAlignment.Center)
+				x -= label.DesiredSize.Width / 2;
+			else if (halign == HorizontalAlignment.Right)
+				x -= label.DesiredSize.Width;
+			Canvas.SetLeft(label, x);
+
+			double y = location.Y;
+			if (valign == VerticalAlignment.Center)
+				y -= label.DesiredSize.Height / 2;
+			else if (valign == VerticalAlignment.Bottom)
+				y -= label.DesiredSize.Height;
+			Canvas.SetTop(label, y);
 		}
 	}
 }
