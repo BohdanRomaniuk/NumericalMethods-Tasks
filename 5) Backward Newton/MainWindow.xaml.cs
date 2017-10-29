e31b3f5b62ab0183505e68eaa62ca6ed98deb4b9
BohdanRomaniuk <bohdan2307@gmail.com>
2017-10-29 19:05:29 +0400

    Update Backward Newton

--- 5) Backward Newton/MainWindow.xaml.cs
+++ 5) Backward Newton/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using NCalc;
 
 namespace _5__Backward_Newton
 {
@@ -20,9 +21,70 @@ namespace _5__Backward_Newton
     /// </summary>
     public partial class MainWindow : Window
     {
+        private NCalc.Expression exp;
+        private double[] vectorX;
+        private double[] vectorY;
+        private double[] polynomial;
         public MainWindow()
         {
             InitializeComponent();
+            exp = new NCalc.Expression(function.Text);
+            string[] xValues = textBox1.Text.Split(' ');
+            vectorX = new double[xValues.Length];
+            vectorY = new double[xValues.Length];
+            for(int i=0; i<xValues.Length; ++i)
+            {
+                vectorX[i] = Convert.ToDouble(xValues[i]);
+            }
+        }
+        private double[] multiplyPolynoms(double[] first, double[] second)
+        {
+            double[] result = new double[first.Length + second.Length - 1];
+            for (int i = 0; i < first.Length; ++i)
+            {
+                for (int j = 0; j < second.Length; ++j)
+                {
+                    result[i + j] += first[i] * second[j];
+                }
+            }
+            return result;
+        }
+        private double calculateFx(double x)
+        {
+            exp.Parameters["X"] = x;
+            return (double)exp.Evaluate();
+        }
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            exp = new NCalc.Expression(function.Text);
+            for(int i=0; i<vectorX.Length; ++i)
+            {
+                vectorY[i] = calculateFx(vectorX[i]);
+                textBox3.Text += vectorY[i] + " ";
+            }
+        }
+        private double calculateSplitSub(int from, int to, double[] x_vector, double[] y_vector)
+        {
+            if(Math.Abs(to-from)==1)
+            {
+                return (y_vector[to] - y_vector[from]) / (x_vector[to] - x_vector[from]);
+            }
+            return (calculateSplitSub(from + 1, to, vectorX, vectorY) - calculateSplitSub(from,to-1, vectorX, vectorY)) / (x_vector[to]  - x_vector[from]);
+        }
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            polynomial = new double[1]{ vectorY[vectorY.Length-1]};
+            int count = 1;
+            for (int i=vectorX.Length; i>=0; --i)
+            {
+                double[] new_pol = new double[2] { 1, -vectorY[vectorY.Length - 1] };
+                for(int j=0; j<count; ++j)
+                {
+
+                }
+                ++count;
+                new_pol = multiplyPolynoms(new_pol, new double[1] { calculateSplitSub(vectorX.Length - 1, i, vectorX, vectorY) });
+            }
         }
     }
 }
