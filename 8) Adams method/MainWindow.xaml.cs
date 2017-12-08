7eded5f5bd2f0659350b4c72e88584b3d6fe44d2
BohdanRomaniuk <bohdan2307@gmail.com>
2017-12-08 21:01:22 +0200

    Add all explicit Adams method formulas for all ranks

--- 8) Adams method/MainWindow.xaml.cs
+++ 8) Adams method/MainWindow.xaml.cs
@@ -42,7 +42,126 @@ namespace _8__Adams_method
             return (double)exact.Evaluate();
         }
 
-        private void button_Click(object sender, RoutedEventArgs e)
+        private void explicitMethod(uint rank)
+        {
+            TextRange txt = new TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd);
+            txt.Text = "";
+            funct = new NCalc.Expression(textBox.Text);
+            exact = new NCalc.Expression(textBox1.Text);
+            double a = Convert.ToDouble(textBox3.Text);
+            double b = Convert.ToDouble(textBox4.Text);
+            double h = Convert.ToDouble(textBox5.Text);
+
+            txt.Text += "Метод Адамса:\n";
+            txt.Text += String.Format("{0}\t{1}\t\t\t{2}\t\t\t{3}\n", "xi", "yi*", "yi", "yi(точне)");
+            double x = a;
+            uint quantity = 0;
+            double k1 = 0, k2 = 0, k3 = 0, k4 = 0;
+            int steps = Convert.ToInt32(textBox6.Text);
+            double[] vals = new double[steps+1];
+            vals[0] = Convert.ToDouble(textBox2.Text);
+            txt.Text += String.Format("{0}\t{1}\t{2}\n", x, vals[0], calculateExact(x));
+
+            switch(rank)
+            {
+                case 2:
+                    {
+                        //#####P=2
+                        for (int i = 1; i < 2; ++i)
+                        {
+                            k1 = h * calculateFunction(x, vals[i - 1]);
+                            k2 = h * calculateFunction(x + h / 3, vals[i - 1] + k1 / 3);
+                            k3 = h * calculateFunction(x + 2 * h / 3, vals[i - 1] - k1 / 3 + k2);
+                            k4 = h * calculateFunction(x + h, vals[i - 1] + k1 - k2 + k3);
+                            vals[i] = vals[i - 1] + (k1 + 3 * k2 + 3 * k3 + k4) / 8;
+                            x += h;
+                            txt.Text += String.Format("{0}\t{1}\t{2}\n", x, vals[i], calculateExact(x));
+                            ++quantity;
+                        }
+                        for (int i = 2; i <= steps; ++i)
+                        {
+                            vals[i] = vals[i - 1] + h * (1.5 * calculateFunction(x, vals[i - 1]) - 0.5 * calculateFunction(x - h, vals[i - 2]));
+                            x += h;
+                            txt.Text += String.Format("{0}\t{1}\t{2}\n", x, vals[i], calculateExact(x));
+                            ++quantity;
+                        }
+                        break;
+                    }
+                case 3:
+                    {
+                        //#####P=3
+                        for (int i = 1; i < 3; ++i)
+                        {
+                            k1 = h * calculateFunction(x, vals[i - 1]);
+                            k2 = h * calculateFunction(x + h / 3, vals[i - 1] + k1 / 3);
+                            k3 = h * calculateFunction(x + 2 * h / 3, vals[i - 1] - k1 / 3 + k2);
+                            k4 = h * calculateFunction(x + h, vals[i - 1] + k1 - k2 + k3);
+                            vals[i] = vals[i - 1] + (k1 + 3 * k2 + 3 * k3 + k4) / 8;
+                            x += h;
+                            txt.Text += String.Format("{0}\t{1}\t{2}\n", x, vals[i], calculateExact(x));
+                            ++quantity;
+                        }
+                        for (int i = 3; i <= steps; ++i)
+                        {
+                            vals[i] = vals[i - 1] + (h / 12) * (23 * calculateFunction(x, vals[i - 1]) - 16 * calculateFunction(x - h, vals[i - 2]) + 5 * calculateFunction(x - 2 * h, vals[i - 3]));
+                            x += h;
+                            txt.Text += String.Format("{0}\t{1}\t{2}\n", x, vals[i], calculateExact(x));
+                            ++quantity;
+                        }
+                        break;
+                    }
+                case 4:
+                    {
+                        //#####P=4
+                        for (int i = 1; i < 4; ++i)
+                        {
+                            k1 = h * calculateFunction(x, vals[i - 1]);
+                            k2 = h * calculateFunction(x + h / 3, vals[i - 1] + k1 / 3);
+                            k3 = h * calculateFunction(x + 2 * h / 3, vals[i - 1] - k1 / 3 + k2);
+                            k4 = h * calculateFunction(x + h, vals[i - 1] + k1 - k2 + k3);
+                            vals[i] = vals[i - 1] + (k1 + 3 * k2 + 3 * k3 + k4) / 8;
+                            x += h;
+                            txt.Text += String.Format("{0}\t{1}\t{2}\n", x, vals[i], calculateExact(x));
+                            ++quantity;
+                        }
+                        for (int i = 4; i <= steps; ++i)
+                        {
+                            vals[i] = vals[i - 1] + (h / 24) * (55 * calculateFunction(x, vals[i - 1]) - 59 * calculateFunction(x - h, vals[i - 2]) + 37 * calculateFunction(x - 2 * h, vals[i - 3]) - 9 * calculateFunction(x - 3 * h, vals[i - 4]));
+                            x += h;
+                            txt.Text += String.Format("{0}\t{1}\t{2}\n", x, vals[i], calculateExact(x));
+                            ++quantity;
+                        }
+                        break;
+                    }
+                case 5:
+                    {
+                        //#####P=5
+                        for (int i = 1; i < 5; ++i)
+                        {
+                            k1 = h * calculateFunction(x, vals[i - 1]);
+                            k2 = h * calculateFunction(x + h / 3, vals[i - 1] + k1 / 3);
+                            k3 = h * calculateFunction(x + 2 * h / 3, vals[i - 1] - k1 / 3 + k2);
+                            k4 = h * calculateFunction(x + h, vals[i - 1] + k1 - k2 + k3);
+                            vals[i] = vals[i - 1] + (k1 + 3 * k2 + 3 * k3 + k4) / 8;
+                            x += h;
+                            txt.Text += String.Format("{0}\t{1}\t{2}\n", x, vals[i], calculateExact(x));
+                            ++quantity;
+                        }
+                        for (int i = 5; i <= steps; ++i)
+                        {
+                            vals[i] = vals[i - 1] + (h / 720) * (1901 * calculateFunction(x, vals[i - 1]) - 2774 * calculateFunction(x - h, vals[i - 2]) + 2616 * calculateFunction(x - 2 * h, vals[i - 3]) - 1274 * calculateFunction(x - 3 * h, vals[i - 4]) + 251 * calculateFunction(x - 4 * h, vals[i - 5]));
+                            x += h;
+                            txt.Text += String.Format("{0}\t{1}\t{2}\n", x, vals[i], calculateExact(x));
+                            ++quantity;
+                        }
+                        break;
+                    }
+            }
+            txt.Text += String.Format("Кількість ітерацій: {0}\n", quantity);
+            txt.Text += String.Format("Точність(|y(b)-y*(b)|): {0}", Math.Abs(vals[steps] - calculateExact(x)));
+        }
+
+        private void implicitMethod(uint rank)
         {
             TextRange txt = new TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd);
             txt.Text = "";
@@ -53,14 +172,15 @@ namespace _8__Adams_method
             double h = Convert.ToDouble(textBox5.Text);
             double yprev = Convert.ToDouble(textBox2.Text);
             double ynext = 0;
-            txt.Text += "Методи Адамса:\n";
+            txt.Text += "Метод Адамса:\n";
             txt.Text += String.Format("{0}\t{1}\t\t\t{2}\t\t\t{3}\n", "xi", "yi*", "yi", "yi(точне)");
             double x = a;
             uint quantity = 0;
             double k1 = 0, k2 = 0, k3 = 0, k4 = 0;
             int steps = Convert.ToInt32(textBox6.Text);
             double yadamsnext = 0, yadamsprev = 0;
-
+            /*
+            double yyy = yprev;
             while (x <= b)
             {
                 k1 = h * calculateFunction(x, yprev);
@@ -68,25 +188,50 @@ namespace _8__Adams_method
                 k3 = h * calculateFunction(x + 2 * h / 3, yprev - k1 / 3 + k2);
                 k4 = h * calculateFunction(x + h, yprev + k1 - k2 + k3);
                 ynext = yprev + (k1 + 3 * k2 + 3 * k3 + k4) / 8;
-                
+
                 yadamsprev = ynext;
-                
+
                 for (int i = 0; i < steps; ++i)
                 {
-                    yadamsnext = yprev + (h / 2) * (calculateFunction(x + h, yadamsprev) + calculateFunction(x, yprev));
+                    yadamsnext = yyy + (h/2)* (calculateFunction(x + h, yadamsprev)+calculateFunction(x, yyy));
                     yadamsprev = yadamsnext;
-                    
                     ++quantity;
                 }
 
                 x += h;
                 txt.Text += String.Format("{0}\t{1}\t{2}\t{3}\n", x, ynext, yadamsnext, calculateExact(x));
                 yprev = ynext;
-                //yprev = yadamsnext;
+                yyy = yadamsnext;
+                yprev = yadamsnext;
                 ++quantity;
             }
+            */
+
+            //while (x <= b)
+            //{
+
+            //    ynext = (yprev + (h / 2) * calculateFunction(x, yprev) / (1 - (h/2) * (x + h) / (Math.Pow(x + h, 2) + 1)));
+            //    x += h;
+            //    txt.Text += String.Format("{0}\t{1}\t{2}\n", x, ynext, calculateExact(x));
+            //    yprev = ynext;
+            //    ++quantity;
+            //}
+
             txt.Text += String.Format("Кількість ітерацій: {0}\n", quantity);
             txt.Text += String.Format("Точність(|y(b)-y*(b)|): {0}", Math.Abs(yadamsnext - calculateExact(x)));
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            uint rank = Convert.ToUInt32(comboBox.SelectedIndex);
+            if(rank<=3)
+            {
+                explicitMethod(rank+2);
+            }
+            else
+            {
+                implicitMethod(rank);
+            }
         }
     }
 }
